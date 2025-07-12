6b18a735b806475ef54fe836dcec672e90bcbb8b
Bruno Caramelo <bruno.caramelo5@gmail.com>
2025-07-12 20:50:58 -0300

    migracoes

---
+++ database/migrations/2025_07_10_182436_create_kpi_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('kpi_items', function (Blueprint $table) {
+            $table->id();
+
+
+            $table->string('titulo');
+            $table->decimal('valor', 10, 2);
+            $table->decimal('variacao_percentual', 10, 2);
+
+            $table->unsignedBigInteger('kpi_type_id');
+
+            $table->timestamps();
+
+            $table->softDeletes();
+
+            $table->foreign('kpi_type_id')
+                  ->references('id')
+                  ->on('kpi_types');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('kpi');
+    }
+};
